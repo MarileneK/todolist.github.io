0ececda42d110887eb0c68a4615d5ba9f50e9ffd
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-11-16 22:46:15 +0100

    function newElement working

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,9 +4,17 @@ console.log(document.getElementById("toDo").textContent);
 function newElement() {
     // event.preventDefault();
     let li = document.createElement("li"); // 1. Create a new <li>
-    let inputValue = document.querySelector("#toDo").value; // 2. Target the input element in which user will enter a todo
+    let inputValue = document.querySelector("#toDo").value; // 2. Get the input from user by targeting the input element
     console.log("inputValue =", inputValue);
 
-    let text = document.createTextNode(inputValue); // 3. Get the user input
+    let text = document.createTextNode(inputValue); // 3. Create a text with user input
     li.appendChild(text); // 4. Append the variable "text" to the <li>
+
+    if (inputValue === '') { // 5. If the user do not writes any text in the input...
+        alert("You must write something");
+    } else {
+        document.querySelector("ul").appendChild(li); // ... else: we append the variable "li" to the <ul>
+    }
+
+    document.querySelector("#toDo").value = "";
 }
