e00ec0e58de1bd2d337d616577e567821de56089
Marilène <67200994+MarileneK@users.noreply.github.com>
2021-11-17 22:26:10 +0100

    trying to add bin icon

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,20 +1,39 @@
-console.log(document.getElementById("toDo").textContent);
-
 // Create a new <li> after clicking on "Add" button
 function newElement() {
-    // event.preventDefault();
-    let li = document.createElement("li"); // 1. Create a new <li>
-    let inputValue = document.querySelector("#toDo").value; // 2. Get the input from user by targeting the input element
+    let li = document.createElement("li"); // 1. Je crée un nouveau <li>
+    let inputValue = document.querySelector("#toDo").value; // 2. Je récupère la valeur saisie par l'utilisateur
     console.log("inputValue =", inputValue);
 
-    let text = document.createTextNode(inputValue); // 3. Create a text with user input
-    li.appendChild(text); // 4. Append the variable "text" to the <li>
+    let text = document.createTextNode(inputValue); // 3. Je crée du texte qui correspond à la valeur saisie par l'utilisateur + picto poubelle
+    li.appendChild(text); // 4. Je rattache ma variable "text" au <li> que j'ai créé
+    console.log("log de li.appendChild(text)", li.appendChild(text));
 
-    if (inputValue === '') { // 5. If the user do not writes any text in the input...
+    if (inputValue === '') { // 5. Si l'utilisateur laisse le champ vide, je l'en informe 
         alert("You must write something");
     } else {
-        document.querySelector("ul").appendChild(li); // ... else: we append the variable "li" to the <ul>
+        document.querySelector("ul").appendChild(li); // ... AUTREMENT : je rattache le <li> au <ul>
     }
 
     document.querySelector("#toDo").value = "";
+
+    let span = document.createElement("span"); // Je crée un <span>
+    // let spanText = document.createTextNode('<i class="far fa-trash-alt"></i>');
+    let spanText = document.createTextNode('\u00D7');
+    span.className = "close";
+    span.appendChild(spanText);
+    li.appendChild(span);
+
+    let close = document.querySelectorAll("close");
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let parentOfClose = this.parentElement;
+            parentOfClose.style.display = "none";
+        }
+    }
 }
+
+// Add a trash on each new <li>
+let liElements = document.querySelector("li");
+
+
+
